975dd7f7b1891f2e64ebd1cb6e77d8c7e5f1b93f
0FiFi0 <115630892+0FiFi0@users.noreply.github.com>
2023-11-06 22:58:13 +0100

    Zadanie 4

---
+++ Controllers/PhotoController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Laboratorium_3___App.Models;
+
+namespace Laboratorium_3___App.Controllers
+{
+    public class PhotoController : Controller
+    {
+        static Dictionary<int, Photo> _photos = new Dictionary<int, Photo>();
+        static int Id = 1;
+        public IActionResult Index()
+        {
+            return View(_photos);
+        }
+        
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Photo model)
+        {
+            if (ModelState.IsValid)
+            {
+                // dodaj model do bazy lub kolekcji
+                model.Id = Id++;
+                _photos.Add(model.Id, model);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Update(int Id)
+        {
+            return View(_photos[Id]);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Photo model)
+        {
+            if (ModelState.IsValid)
+            {
+                _photos[model.Id] = model;
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details(int Id)
+        {
+            return View(_photos[Id]);
+        }
+
+        [HttpPost]
+        public IActionResult Details(Photo model)
+        {
+            if (ModelState.IsValid)
+            {
+                _photos[model.Id] = model;
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        {
+            return View(_photos[Id]);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Photo model)
+        {
+            _photos.Remove(model.Id);
+            return RedirectToAction("Index");
+        }
+    }
+}
